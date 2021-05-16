b44a7120b044f29ed55f29124f68d49a7e1b1890
HutskoAntonEpam <61325582+HutskoAntonEpam@users.noreply.github.com>
2021-05-16 16:50:39 +0300

    course added

---
+++ course/app/src/main/java/com/example/my_hotel/activity/ReservationActivity.java
@@ -0,0 +1,80 @@
+package com.example.my_hotel.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.my_hotel.R;
+import com.example.my_hotel.main.MainActivity;
+
+public class ReservationActivity extends AppCompatActivity {
+
+    CalendarView calender;
+    TextView date_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+        calender = (CalendarView) findViewById(R.id.calender);
+        date_view = (TextView) findViewById(R.id.date_view);
+
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String Date
+                        = dayOfMonth + "-"
+                        + (month + 1) + "-" + year;
+                // установить эту дату в TextView для отображения
+                date_view.setText(Date);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_info_hotel:
+                Intent hotel = new Intent(ReservationActivity.this, HotelActivity.class);
+                startActivity(hotel);
+                return true;
+            case R.id.action_info_program:
+                Intent main = new Intent(ReservationActivity.this, MainActivity.class);
+                startActivity(main);
+                return true;
+            case R.id.action_location:
+                Intent location = new Intent(ReservationActivity.this, LocationActivity.class);
+                startActivity(location);
+                return true;
+            case R.id.action_room:
+                Intent gallery = new Intent(ReservationActivity.this, GalleryActivity.class);
+                startActivity(gallery);
+                return true;
+            case R.id.action_settings:
+                //infoTextView.setText("action_settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+}
